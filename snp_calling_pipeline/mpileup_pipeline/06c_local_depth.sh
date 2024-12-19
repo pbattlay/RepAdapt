9e54547d007939e148eae457b59b6f5a9a236234
pbattlay <63684292+pbattlay@users.noreply.github.com>
2024-12-19 12:36:10 +1100

    Update 06c_local_depth.sh
    Fixed sorting issues–files are initially sorted based on reference index (FAI file) do bedtools map no longer throws an error.
    
    Bedtools map is also provided with a "genome file" via the -g flag, which makes it okay with BED files that miss some reference scaffolds/chromosomes. (e.g., a gene BED file may not have a gene on every scaffold).

--- snp_calling_pipeline/mpileup_pipeline/06c_local_depth.sh
+++ snp_calling_pipeline/mpileup_pipeline/06c_local_depth.sh
@@ -35,21 +35,29 @@ module load bedtools
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ]
 then
 
-# make a BED file (and location list) of 5000 bp windows from FASTA index
-cat $GENOMEFOLDER/${GENOME}.fai | awk -v w=5000 '{chr = $1; chr_len = $2;
+# make a genomefile for bedtools
+awk '{print $1"\t"$2}' $GENOMEFOLDER/${GENOME}.fai > 02_info_files/genome.bed
+
+# make a BED file of 5000 bp windows from FASTA index
+awk -v w=5000 '{chr = $1; chr_len = $2;
     for (start = 0; start < chr_len; start += w) {
         end = ((start + w) < chr_len ? (start + w) : chr_len);
         print chr "\t" start "\t" end;
     }
-}' | sort -u -k1,1 -k2,2n > 02_info_files/windows.bed
+}' $GENOMEFOLDER/${GENOME}.fai > 02_info_files/windows.bed
 
-cat 02_info_files/windows.bed | awk -F "\t" '{print $1":"$2"-"$3}' | sort -k1,1 > 02_info_files/windows.list
+# now make location list from window bedfile and sort for join
+awk -F "\t" '{print $1":"$2"-"$3}' 02_info_files/windows.bed | sort -k1,1 > 02_info_files/windows.list
 
-# and a bed file (and location list) of each gene
-cat $GENOMEFOLDER/$ANNOTATION | awk '$3 == "gene" {print $1"\t"$4"\t"$5}' \
-| sort -u -k1,1 -k2,2n > 02_info_files/genes.bed
+# and a bed file of each gene
+awk '$3 == "gene" {print $1"\t"$4"\t"$5}' $GENOMEFOLDER/$ANNOTATION | uniq > 02_info_files/genes.bed
 
-cat 02_info_files/genes.bed | awk -F "\t" '{print $1":"$2"-"$3}' | sort -k1,1 > 02_info_files/genes.list
+# we also sort this file based on the order of the reference index
+cut -f1 $GENOMEFOLDER/${GENOME}.fai | while read chr; do awk -v chr=$chr '$1 == chr {print $0}' 02_info_files/genes.bed | sort -k2,2n; done > 02_info_files/genes.sorted.bed
+mv 02_info_files/genes.sorted.bed 02_info_files/genes.bed
+
+# now make location list from sorted gene bedfile and sort for join
+awk -F "\t" '{print $1":"$2"-"$3}' 02_info_files/genes.bed | sort -k1,1 > 02_info_files/genes.list
 
 fi
 
@@ -57,31 +65,29 @@ fi
 file=$(cut -f2 02_info_files/datatable.txt | sort | uniq | sed "${SLURM_ARRAY_TASK_ID}q;d")
 bamfile=${file}.realigned.bam
 
+# dump depth of coverage at every position in the genome
 samtools depth -aa $ALIGNEDFOLDER/$bamfile > $SVFOLDER/${file}.depth
 
 # gene depth analysis
 echo \n">>> Computing depth of each gene for $file <<<"\n
-cat $SVFOLDER/${file}.depth | awk '{print $1"\t"$2"\t"$2"\t"$3}' | bedtools map -a 02_info_files/genes.bed -b stdin -c 4 -o mean -null 0 | awk -F "\t" '{print $1":"$2"-"$3"\t"$4}' | sort -k1,1 > $SVFOLDER/${file}-genes.tsv
+awk '{print $1"\t"$2"\t"$2"\t"$3}' $SVFOLDER/${file}.depth | bedtools map -a 02_info_files/genes.bed -b stdin -c 4 -o mean -null 0 -g 02_info_files/genome.bed | awk -F "\t" '{print $1":"$2"-"$3"\t"$4}' | sort -k1,1 > $SVFOLDER/${file}-genes.tsv
 
 # sort gene depth results based on input bed file
 join -a 1 -e 0 -o '1.1 2.2' -t $'\t' 02_info_files/genes.list $SVFOLDER/${file}-genes.tsv > $SVFOLDER/${file}-genes.sorted.tsv
 
 # window depth analysis
 echo \n">>> Computing depth of each window for $file <<<"\n
-cat $SVFOLDER/${file}.depth | awk '{print $1"\t"$2"\t"$2"\t"$3}' | bedtools map -a 02_info_files/windows.bed -b stdin -c 4 -o mean -null 0 | awk -F "\t" '{print $1":"$2"-"$3"\t"$4}' | sort -k1,1  > $SVFOLDER/${file}-windows.tsv
+awk '{print $1"\t"$2"\t"$2"\t"$3}' $SVFOLDER/${file}.depth | bedtools map -a 02_info_files/windows.bed -b stdin -c 4 -o mean -null 0 -g 02_info_files/genome.bed | awk -F "\t" '{print $1":"$2"-"$3"\t"$4}' | sort -k1,1  > $SVFOLDER/${file}-windows.tsv
 
 # sort window depth results based on input bed file
 join -a 1 -e 0 -o '1.1 2.2' -t $'\t' 02_info_files/windows.list $SVFOLDER/${file}-windows.tsv > $SVFOLDER/${file}-windows.sorted.tsv
 
 # overall genome depth
 echo \n">>> Computing depth of whole genome for $file <<<"\n
-cat $SVFOLDER/${file}.depth | awk '{sum += $3; count++} END {if (count > 0) print sum/count; else print "No data"}' > $SVFOLDER/${file}-wg.txt
+awk '{sum += $3; count++} END {if (count > 0) print sum/count; else print "No data"}' $SVFOLDER/${file}.depth > $SVFOLDER/${file}-wg.txt
 
 echo " >>> Cleaning a bit...
 "
 rm -rf $SVFOLDER/${file}.depth
 echo "
 DONE! Check your files"
-
-
-
