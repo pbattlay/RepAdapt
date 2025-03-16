dbbe9c825364443eefe4f8c5496e082a8847a742
pbattlay <63684292+pbattlay@users.noreply.github.com>
2025-03-17 07:31:24 +1100

    Update 08_scaffoldVCF_filtering.sh
    RepAdapt 2025 filtering parameters added

--- snp_calling_pipeline/mpileup_pipeline/08_scaffoldVCF_filtering.sh
+++ snp_calling_pipeline/mpileup_pipeline/08_scaffoldVCF_filtering.sh
@@ -8,8 +8,7 @@
 #SBATCH --mem=16G
 #SBATCH --time=00-12:00:00
 
-module load vcftools bcftools
-module load StdEnv/2020 intel/2020.1.217 tabix/0.2.6
+module load samtools vcftools bcftools parallel
 
 cd $SLURM_SUBMIT_DIR
 
@@ -32,33 +31,16 @@ REGION_FILE=$(ls 02_info_files/all_scafs*pos | sed "${SLURM_ARRAY_TASK_ID}q;d")
     echo "
     >>> Filtering through BCFtools first!
     "
-    parallel -j8 "bcftools filter -e 'MQ < 30' $VCF/${DATASET}_{}.vcf -Oz > $FILTVCF/${DATASET}_{}_filteredTmp.vcf.gz" :::: $REGION_FILE
-
-    echo "
-    >>> Filtering through VCFtools now!!
-    "
-    parallel -j8 "vcftools --gzvcf $FILTVCF/${DATASET}_{}_filteredTmp.vcf.gz \
-        --minQ 30 \
-        --minGQ 20 \
-        --minDP 5 \
-        --max-alleles 2 \
-        --max-missing 0.7 \
-        --recode \
-        --stdout > $FILTVCF/${DATASET}_{}_filtered.vcf" :::: $REGION_FILE
+    parallel -j8 "bcftools filter -e 'AC=AN || MQ < 30' $VCF/${DATASET}_{}.vcf -Oz > $FILTVCF/${DATASET}_{}_filtered.vcf.gz" :::: $REGION_FILE
 
     echo "
     >>> Preparation for concatenation of VCF files
     "
-    parallel -j8 "bgzip $FILTVCF/${DATASET}_{}_filtered.vcf" :::: $REGION_FILE
     parallel -j8 "tabix -p vcf $FILTVCF/${DATASET}_{}_filtered.vcf.gz" :::: $REGION_FILE
 
 echo "
 >>> Cleaning a bit...
 "
-for scaf in $(cat $REGION_FILE)
-do
-  rm $FILTVCF/${DATASET}_${scaf}_filteredTmp.vcf.gz
-done
 
 echo "
 DONE! Check you files"
